2cda8d9776beb7929f51bdc0bd96696e110ed571
huanghuanlai <102535481@qq.com>
2016-09-02 18:31:03 +0800

    init

--- auinput.sh
+++ auinput.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/expect
-spawn docker run -ti --privileged -p 3330-3333:3330-3333 -e "container=docker" dounine/centos-openvpn /sbin/init
+spawn docker run -ti --privileged -p 3330-3333:3330-3333 -e "container=docker" dounine/centos-openvpn:1.4 /sbin/init
 expect "login:"
 send "root\r"
 expect "Password:"
 send "root\r"
 send "systemctl restart iptables.service\r"
 send "iptables -F\r"
-send "iptables -t nat -A POSTROUTING -s 10.8.0.0/32 -o eth0 -j MASQUERADE\r"
-send "vpn\r"
-#send "printf \"\ec\"\r"
-send "clear\r"
-expect eof
+send "iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT\r"
+send "iptables -A FORWARD -s 10.8.0.0/24 -j ACCEPT\r"
+send "iptables -A FORWARD -j REJECT\r"
+send "iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE\r"
+send "reset\r"
 interact
